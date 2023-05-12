171c5662b7e09ea3babb8fe046cbef9239722d71
npoquiz <126127826+npoquiz@users.noreply.github.com>
2023-05-11 18:18:22 -0700

    Delete CSC123-Assignment7 directory

--- CSC123-Assignment7/Bank/com/usman/csudh/bank/core/InsufficientBalanceException.java
+++
@@ -1,9 +0,0 @@
-package com.usman.csudh.bank.core;
-
-public class InsufficientBalanceException extends Exception {
-
-	public InsufficientBalanceException(String message) {
-		super(message);
-	}
-
-}
--- CSC123-Assignment7/Bank/com/usman/csudh/bank/core/NoSuchAccountException.java
+++
@@ -1,9 +0,0 @@
-package com.usman.csudh.bank.core;
-
-public class NoSuchAccountException extends Exception {
-
-	public NoSuchAccountException(String message) {
-		super(message);
-	}
-
-}
